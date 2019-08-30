f7cec9e8e985d2d11b2955900736cfd0dd9cbe2a
jason <jasonedlewis@gmail.com>
2019-08-30 12:40:40 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ let addToy = false
 
 // YOUR CODE HERE
 
-const mainDiv = document.getElementById('toy-header');
+const mainDiv = document.querySelector('#toy-collection');
     fetch('http://localhost:3000/toys')
        .then(resp => resp.json())
        .then(toys => setUpDivs(toys));
@@ -13,7 +13,7 @@ const mainDiv = document.getElementById('toy-header');
 
 const setUpDivs =(toys)=>{
   toys.forEach(toy => {
-    let newPost =    mainDiv.innerHTML += divs(toy.id, toy.name, toy.image, toy.likes);
+   mainDiv.innerHTML += divs(toy.id, toy.name, toy.image, toy.likes);
   }
     
   );
@@ -25,8 +25,8 @@ const divs =(id,name, image,likes)=>{
   return `<div class="card" data-id=${id}>
   <h2 data-toy-name=${name}>${name}</h2>
   <img class="toy-avatar" src=${image} />
-  <p data-likes=${likes}>likes: ${likes}</p>
-  <button class="like-btn">💗</button>
+  <p data-likes=${likes}>likes:${likes}</p>
+  <button class="like-btn">😍</button>
   </div>`
 }
 // console.log(grabToys());
@@ -43,3 +43,36 @@ addBtn.addEventListener('click', () => {
 
 
 // OR HERE!
+
+const form = document.querySelector(".add-toy-form");
+const theText = form[0];
+const theImg = form[1];
+
+
+let whatsTheValue =(event)=>{
+  event.preventDefault();
+  debugger
+  console.log(theText);
+  // form.reset()
+  }
+
+
+
+
+
+// const addNewToy =(name, url)=>{
+//   fetch("http://localhost:3000/toys",
+//   {
+//     method: 'POST',
+//     headers: {
+//       "Content - Type" : "application/json",
+//       "Accept" : "application/json"
+//     },
+//     body:  JSON.stringify({
+      
+//     })
+//   }).then(resp => resp.json()).then(toys => setUpDivs(toys) )
+
+// };
+
+form.addEventListener('submit', whatsTheValue)
