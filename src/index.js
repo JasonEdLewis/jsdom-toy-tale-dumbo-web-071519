7a1e4b820309ed362dc3e54e473d41e980d1e71d
jason <jasonedlewis@gmail.com>
2019-08-30 11:08:47 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,33 @@ let addToy = false
 
 // YOUR CODE HERE
 
+const mainDiv = document.getElementById('toy-header');
+    fetch('http://localhost:3000/toys')
+       .then(resp => resp.json())
+       .then(toys => setUpDivs(toys));
+
+
+
+const setUpDivs =(toys)=>{
+  toys.forEach(toy => {
+    let newPost =    mainDiv.innerHTML += divs(toy.id, toy.name, toy.image, toy.likes);
+  }
+    
+  );
+
+
+}
+
+const divs =(id,name, image,likes)=>{
+  return `<div class="card" data-id=${id}>
+  <h2 data-toy-name=${name}>${name}</h2>
+  <img class="toy-avatar" src=${image} />
+  <p data-likes=${likes}>likes: ${likes}</p>
+  <button class="like-btn">💗</button>
+  </div>`
+}
+// console.log(grabToys());
+
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
   addToy = !addToy
